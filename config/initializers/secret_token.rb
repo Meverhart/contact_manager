ce433ee6f998bcfa3a6d2c6c2deb3f6c8f64a8af
Meverhart <mark.a.everhart@gmail.com>
2014-02-04 13:22:22 -0500

    Generate initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = '7de8cb7b82b99ca39b6c66f7071ec5ef53c4f61414c1b4d1434ed14b74e1e3ff79d14e6c4b5001956b9af49a09faffb993f552b56ba8a37a3f8a1dd6c431b6bd'
---
+++ lib/tasks/dev_server.rake
@@ -0,0 +1,8 @@
+task :server do
+  port = ENV['PORT'] ? ENV['PORT'] : '3000'
+  puts 'start unicorn development'
+
+  sh "cd #{Rails.root} && RAILS_ENV=development unicorn -p #{port}"
+end
+
+task :s => :server
